f8bd0cfeec1e7780e1d1e3f4bba09bfab400ba11
Luka_Hat <luka.hat58@gmail.com>
2023-11-22 22:51:59 +0100

    add quote api functionality

--- src/QuoteGen/QuoteGen.js
+++ src/QuoteGen/QuoteGen.js
@@ -1,35 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export const QuoteGen = () => {
-  const [quote, setQuote] = useState(
-    "The map of the world is drawn by travelers and nomads. Built into it are steps, nights and days, stations and encounters."
+  const [isPoloModalOpen, setIsPoloModalOpen] = useState(false);
+  const [isKhanModalOpen, setIsKhanModalOpen] = useState(false);
+
+  const [poloFact, setPoloFact] = useState(
+    "Jeste li znali da se obitelj Marca Pola bavila trgovinom, a on je s njima krenuo na putovanje u Aziju sa 17 godina?"
+  );
+  const [khanFact, setKhanFact] = useState(
+    "Jeste li znali da je Kublai-Khan bio unuk Džingis-kana?"
   );
 
-  useEffect(() => {
-    // Make a GET request when the component mounts
-    axios
-      .get("https://put-svile-backend.onrender.com/api/v1/quotes/single-quote")
-      .then((response) => {
-        const singleFact = response.data.singleFact[0];
-        if (singleFact && singleFact.lang) {
-          // Display the English version of the quote (change index as needed)
-          setQuote(singleFact.lang[1]);
-        } else {
-          setQuote(
-            "The map of the world is drawn by travelers and nomads. Built into it are steps, nights and days, stations and encounters."
-          );
-        }
-        console.log("Response:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error:", error.message);
-      });
-  }, []); // The empty dependency array ensures that the effect runs only once, equivalent to componentDidMount
+  const quoteLink = "https://put-svile-backend.onrender.com/api/v1/quotes";
+
+  const randNumber = () => Math.floor(Math.random() * 30);
+  const index = randNumber();
+
+  const getPoloQuote = async () => {
+    try {
+      const response = await axios.get(`${quoteLink}?isAbout=marco-polo`);
+      setPoloFact(response.data.facts[index].fact);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  const getKhanQuote = async () => {
+    try {
+      const response = await axios.get(`${quoteLink}?isAbout=kublai-khan`);
+      setKhanFact(response.data.facts[index].fact);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  const handlePoloClick = () => {
+    getPoloQuote();
+    setIsPoloModalOpen(true);
+  };
+
+  const handleKhanClick = () => {
+    getKhanQuote();
+    setIsKhanModalOpen(true);
+  };
 
   return (
     <>
-      <h1>{quote}</h1>
+      <section className="quote-boxes">
+        <img
+          className="quote-img"
+          src={`${process.env.PUBLIC_URL + "/assets/krimg/marko.jpg"}`}
+          alt="Marko Polo"
+          onClick={handlePoloClick}
+        ></img>
+        <img
+          className="quote-img"
+          src={`${process.env.PUBLIC_URL + "/assets/krimg/kubla.jpg"}`}
+          alt="Kublaj Khan"
+          onClick={handleKhanClick}
+        ></img>
+      </section>
+
+      <div className={isPoloModalOpen ? "modal polo" : "modal-closed"}>
+        <h2>{poloFact}</h2>
+        <button
+          className="close-button"
+          onClick={() => setIsPoloModalOpen(false)}
+        >
+          X
+        </button>
+      </div>
+
+      <div className={isKhanModalOpen ? "modal khan" : "modal-closed"}>
+        <h2>{khanFact}</h2>
+        <button
+          className="close-button"
+          onClick={() => setIsKhanModalOpen(false)}
+        >
+          X
+        </button>
+      </div>
     </>
   );
 };
